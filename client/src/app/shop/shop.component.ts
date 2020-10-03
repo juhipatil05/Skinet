c2351855f65a225186628af08800b552ab64f5cd
juhipatil05 <juhipatil0394@gmail.com>
2020-10-03 17:26:48 +0530

    End of section 12

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -12,7 +12,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  @ViewChild('search', {static: true}) searchTeam: ElementRef;
+  @ViewChild('search', {static: false}) searchTeam: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
